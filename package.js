8e7c8d363ce2ba8074096c5f2f3b30feac60fef9
kaaposc <kaaposc@gmail.com>
2014-06-26 13:47:14 +0300

    Update package.js
    Npm dep webshot upped to latest 0.15.0 version

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   summary: "Webshot - NPM Screenshot Library using PhantomJS, repackaged For Meteor"
 });
 
-Npm.depends({webshot: "0.5.0"});
+Npm.depends({webshot: "0.15.0"});
 
 Package.on_use(function (api) {
   api.add_files("lib/webshot.js", "server");
-  api.export(['WEBSHOT'], ['server']);
+  api.export('WEBSHOT');
 });
